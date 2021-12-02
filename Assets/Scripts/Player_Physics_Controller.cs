38a1ada427af5bc74e590cb91c8903076a7af3d5
lmonrroy03 <71279825+lmonrroy03@users.noreply.github.com>
2021-12-01 18:44:25 -0800

    Base Geometry Generator & Player Controller

---
+++ Assets/Scripts/Player_Physics_Controller.cs
@@ -0,0 +1,143 @@
+
+/* 
+ * Description: Player movement controller, that also stores jump physics data for how far a certain jump goes in the x and y direction, given the hold time for the space bar. 
+ * 
+ * Reason for this: In order for the geometry generator to create playable levels, it needs to know how far a player can jump given how long the player may press the space bar
+ *              so that it can correctly space out the geometry to maximize its challenge. 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Physics_Controller : MonoBehaviour
+{
+    
+    private float spaceButtonPressLength;
+    private float buttonPressStartTime;
+    private int jumpType;
+    private float playerJumpVel;
+    [HideInInspector]
+    public float movementXSpeed;
+
+    //Physics data that geometry generator uses. 
+    [HideInInspector]
+    public float shortJumpDuration; 
+    
+    [HideInInspector]
+    public float mediumJumpDuration;
+
+    [HideInInspector]
+    public float longJumpDuration;
+
+    public float shortJump;
+    public float mediumJump;
+    public float longJump;
+    private Rigidbody2D RB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementXSpeed = 5.0f;
+        RB = GetComponent<Rigidbody2D>();
+        spaceButtonPressLength = 0.0f;
+        playerJumpVel = 0.0f;
+
+        //calculating jump durations given the heights of each type of jump.
+        shortJumpDuration = CalculateJumpDuration(shortJump);
+        mediumJumpDuration = CalculateJumpDuration(mediumJump);
+        longJumpDuration = CalculateJumpDuration(longJump);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Calculating the time for a space bar press 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            buttonPressStartTime = Time.time;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            RB.velocity = new Vector2(movementXSpeed, RB.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            RB.velocity = new Vector2(-movementXSpeed, RB.velocity.y);
+        }
+
+        //Determing the type of  jump the player will do.
+        if (Input.GetKey(KeyCode.Space) == true && buttonPressStartTime != 0.0f)
+        {
+            spaceButtonPressLength = Time.time - buttonPressStartTime;
+            jumpType = TypeOfJump(spaceButtonPressLength);
+
+            //Doing the jump by calculating its velocity given the jump that is based from the space bar hold length.
+            switch (jumpType)
+            {
+                case 0:
+                    playerJumpVel = CalculateJumpVelocity(shortJumpDuration/2.0f);
+                    break;
+                case 1:
+                    playerJumpVel = CalculateJumpVelocity(mediumJumpDuration / 2.0f);
+                    break;
+                case 2:
+                    playerJumpVel = CalculateJumpVelocity(longJumpDuration / 2.0f);
+                    break;
+                default:
+                    playerJumpVel = 0.0f;
+                    break;
+            }
+
+            playerJumpVel = Mathf.Lerp(RB.velocity.y, playerJumpVel, 0.5f);
+
+            RB.velocity = new Vector2(RB.velocity.x, playerJumpVel);
+
+            if (spaceButtonPressLength >= 1.0f)
+                buttonPressStartTime = 0.0f;
+        }
+        
+    }
+
+    //Returns 0 = short, 1 = medium, 2 = long (JUMPS)
+    public int TypeOfJump(float buttonPressLength)
+    {
+        if (spaceButtonPressLength <= 0.25f)
+        {
+            return 0; //short jump
+        }
+        else
+        {
+            if (spaceButtonPressLength > 0.25f && spaceButtonPressLength <= 0.75f)
+            {
+                return 1; //medium jump
+            }
+            else
+            {
+                if (spaceButtonPressLength > 0.75f)
+                    return 2; //long jump
+            }
+        }
+
+        return -1;//Invalid return but just in case any of the other checks fail.
+    }
+
+    //Calculating jump duration based on given height.
+    public float CalculateJumpDuration(float height)
+    {
+        //This calculates from start to peak.
+        float time = 0.0f;
+
+        time = Mathf.Sqrt(Mathf.Abs(height / (.5f * Physics.gravity.y)));
+
+        return 2 * time; //Doubling the start to peak time because it wont change going down.
+    }
+
+    //Calculating initial velocity of the player from the given jump height.
+    private float CalculateJumpVelocity(float jumpDuration)
+    {
+        return Mathf.Abs(Physics.gravity.y * jumpDuration);
+    }
+}
--- Assets/Scripts/RhythmGroup.cs
+++ Assets/Scripts/RhythmGroup.cs
@@ -1,49 +1,68 @@
+/*
+ * Description: Step 1 in generating level. This is a small section of a level, that contains a beat pattern.
+ *              Each rhythm group contains a length, density and beat pattern.
+ * 
+ * 
+ * 
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Verbs used for the grammar-based approach algorithm
 public enum ACTION
 { 
     MOVE,
     JUMP,
+    WAIT,
 }
+
 
 public class RhythmGroup
 {
-    public List<Rhythm> rhythms = new List<Rhythm>();
-    public float totalTime;
+    public List<Rhythm> rhythms = new List<Rhythm>(); //beat pattern
+    public float totalTime; //lenght of the beat pattern
 
+    //Constructor for rhythm group containing the beat pattern.
     public RhythmGroup(float totalTime_)
     {
         totalTime = totalTime_;
     }
 
+    //This adds a rhythm to the rhythm group.
     public bool AddRhythm(ACTION action_, float start_, float end_)
     {
+        //Check if start time of the beat is in the time this rhythm group exists in.
         if (start_ > totalTime)
         {
             return false;
         }
 
+        //Check if the end time of this rhythm is longer than when the rhythm group is set to end.
         if (end_ > totalTime && action_ != ACTION.JUMP)
         {
             end_ = totalTime;
         }
 
+        //If here we passed the criteria to add the rhythm.
+
+        //Creating the rhythm to then add to the group.
         Rhythm rhythm = new Rhythm(action_, start_, end_);
 
         rhythms.Add(rhythm);
-
+        
         return true;
     }
 }
 
 public class Rhythm
 {
-    public ACTION action;
-    public float start;
-    public float end;
+    public ACTION action;//Verb of the rhythm. I.E is the rhythm a jump or move action?
+    public float start; //Where in the beat pattern it exists. 
+    public float end; //The duration of the beat.
 
+    //Constructor for rhythm
     public Rhythm(ACTION action_, float start_, float end_)
     {
         action = action_;
